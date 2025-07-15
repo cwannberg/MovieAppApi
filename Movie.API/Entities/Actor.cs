f0e4f496dbf5ca0b9f9e4c2fc98a4e36611912c9
Cilla <ceciliawannberg@gmail.com>
2025-07-15 06:59:23 +0200

    Add entities

---
+++ Movie.API/Entities/Actor.cs
@@ -0,0 +1,9 @@
+﻿namespace Movie.API.Entities;
+
+public class Actor
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public int BirthYear { get; set; }
+    public ICollection<Movie> Movies { get; set; } = new List<Movie>();
+}
---
+++ Movie.API/Entities/Movie.cs
@@ -0,0 +1,21 @@
+﻿namespace Movie.API.Entities;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public int Year { get; set; }
+    public string? Duration { get; set; }
+
+    //1-1
+    public int GenreId { get; set; }
+    public Genre Genre { get; set; } = null!;
+
+    //1-1
+    public MovieDetails MovieDetails { get; set; } = null!;
+
+    //1-M
+    public ICollection<Review> Reviews { get; set; } = new List<Review>();
+    //N-M
+    public ICollection<Actor> Actors { get; set; } = new List<Actor>();
+}
